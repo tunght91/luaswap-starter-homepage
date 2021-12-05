cfc2bc2ea0e0acfedb02483f29539ebbe807e80d
tinhbeng <tinhbeng1092@gmail.com>
2021-12-05 21:06:52 +0700

    Update style

--- assets/js/js.js
+++ assets/js/js.js
@@ -38,6 +38,32 @@ jQuery(document).ready(function ($) {
         e.preventDefault();
         $(this).find('> ul').toggleClass('dropdown');
     })
+
+    $('.project-box').slick({
+        dots: true,
+        rows: 2,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
     $(document).mouseup(function (e) {
         var container = $(".navigation.show");
         var icon = $('.icon-mobile');
